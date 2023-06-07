f8c2fac04ec139a785a3c8410f67f476b50e2c5f
Francois Huyghe <francois@13millieconds.com>
2023-06-07 16:41:42 -0400

    Add discernable name to links

--- frontend/components/ProjectCard.jsx
+++ frontend/components/ProjectCard.jsx
@@ -7,7 +7,7 @@ export default function ProjectCard({project}) {
   return (
     <section className='project-card md:flex items-end mb-10 even:flex-row-reverse md:saturate-0 hover:saturate-100 transition-[filter] duration-1000'>
       <div className="image md:mb-6 mb-2">
-      <Link href={`/project/${project.slug.current}`}>
+      <Link href={`/project/${project.slug.current}`} name={project.title}>
         { project.featuredImage &&
           <Image value={project.featuredImage}/>}
         </Link>
